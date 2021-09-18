7f263539781d3a91c9709fabb6f56e64655e46ce
zhongmingpeng <2665850455@qq.com>
2021-09-18 14:39:44 +0800

    second commit

--- src/main/java/com/coder/test/GitTest.java
+++ src/main/java/com/coder/test/GitTest.java
@@ -3,5 +3,6 @@ package com.coder.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("first dog");
+        System.out.println("second dog");
     }
 }
